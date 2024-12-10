22b07fdf8a94312a6304d8ff348b875cdabb8fd6
Deepesh <sainideepesh12@gmail.com>
2024-12-10 12:44:17 +0530

    quizApp

---
+++ question.js
@@ -0,0 +1,29 @@
+const data = [
+    {
+        q: "What is the capital of india?",
+        a: "New Delhi",
+        opt: ["Mumbai", "New Delhi", "kolkata", "Chennai"],
+    },
+    {
+        q: "Who is the president of india?",
+        a: "Draupadi Murmu",
+        opt: ["Narendar modi", "Rahul gandhi", "Draupadi Murmu", "Amit Shah"],
+    },
+    {
+        q: "Which city is called the financial capital of india?",
+        a: "Mumbai",
+        opt: ["Mumbai", "New Delhi", "kolkata", "Chennai"],
+    },
+    {
+        q: "What is the mumbai film industry commonly called?",
+        a: "Bollywood",
+        opt: ["Bollywood", "Hollyywood", "Tollywood", "Jollywood"],
+    },
+    {
+        q: "Which of these is not a programming language?",
+        a: "Quick",
+        opt: ["Quick", "C", "C++", "Python"],
+    },
+    
+];
+export default data;
---
+++ script.js
@@ -0,0 +1,95 @@
+import data from "./question.js";
+const questiondiv = document.querySelector(".question");
+const options = document.querySelectorAll(".option");
+const mainOption = document.querySelector("#options");
+const startdiv = document.querySelector("#start");
+const startdivbutton = document.querySelector("#start button");
+const quizdiv = document.querySelector("#quiz");
+const timer = document.querySelector(".timer")
+const score = document.querySelector("#score")
+let time = 5;
+let useranswer = [];
+let dummy = [];
+let question = null
+
+
+
+startdivbutton.addEventListener("click", startQuiz);
+
+options.forEach((option) => {
+    option.addEventListener("click", storeUserAnswer);
+    
+
+});
+
+function storeUserAnswer(e) {
+    useranswer.push(e.target.innerHTML);
+    mainOption.classList.add("gayab")
+    
+    console.log(useranswer);
+
+    
+}
+
+function startQuiz() {
+    startdiv.classList.add("gayab");
+    quizdiv.classList.remove("gayab");
+    showquestionoptions();
+    startTime()
+}
+function showquestionoptions() {
+    questiondiv.innerHTML = data[selectquestion()].q;
+    options.forEach((option, index) => {
+        option.innerHTML = data[question].opt[index];
+    });
+    mainOption.classList.remove("gayab")
+}
+
+function startTime() {
+    timer.innerHTML = time
+    let a = setInterval(() => {
+        if (time == 1) {
+            time = 5;
+            timer.innerHTML = time
+
+            if (dummy.length == 5) {
+                quizdiv.classList.add("gayab");
+                calculateScore()
+                score.classList.remove("gayab");
+                clearInterval(a)
+            }
+            else {
+                
+                showquestionoptions()
+            }
+        }
+        else {
+            timer.innerHTML = --time
+        }
+    }, 1000)
+}
+
+function calculateScore() {
+    let finalScore = 0;
+    useranswer.forEach((answer, index) => {
+        if (answer == data[index].a)
+            finalScore++;
+    });
+    score.children[0].innerHTML =
+        "Your score is " + finalScore + "out of " + data.length;
+}
+
+function selectquestion() {
+    question = Math.floor(Math.random() * data.length);
+
+    if (dummy.includes(question)) return selectquestion()
+    else {
+        console.log(question)
+        dummy.push(question)
+        return question
+    }
+}
+
+
+
+
